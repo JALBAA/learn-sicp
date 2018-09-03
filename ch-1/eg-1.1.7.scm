6886e5438a2fea1daa1b43f067ec6ec9fe5490ac
zhaoye <zhaoye-ds1@gomeplus.com>
2018-09-03 14:14:56 +0800

    init

---
+++ ch-1/eg-1.1.7.scm
@@ -0,0 +1,23 @@
+; x/(y * y)  + 2y
+; ___________
+;     3
+; y是x的立方根的一个近似值
+
+(define (abs num) 
+    (if (< num 0) 
+        (- num)
+        num
+    )
+)
+(define (improve num target) 
+   (/ (+ (/ num (* target target)) (* 2 target)) 3)
+;    (if (< (abs (- newTarget target)) 0.001) 
+)
+
+(define (lifang num guessNum prev)
+    (if (< (abs (- guessNum prev)) 0.001)
+        guessNum
+        (lifang num (improve num guessNum) guessNum)
+    )
+)
+(display (lifang 8.0 1 0) )
